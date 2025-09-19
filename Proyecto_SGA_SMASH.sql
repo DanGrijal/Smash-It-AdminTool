df6771075c64355503297b9369df85b2b66f24b0
Ripichip <105686182+Reapson@users.noreply.github.com>
2025-09-18 21:58:25 -0600

    Actualizacion BD

--- Proyecto_SGA_SMASH.sql
+++ Proyecto_SGA_SMASH.sql
@@ -74,7 +74,7 @@ CREATE TABLE Cliente (
     nombre VARCHAR(100) NOT NULL,
     correo VARCHAR(100) UNIQUE,
     telefono VARCHAR(20),
-    fecha_registro DATETIME
+    FechaRegistro DATETIME
 );
 
 -- Tabla Reservacion
